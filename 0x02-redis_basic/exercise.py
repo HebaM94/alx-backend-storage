2f1558461653a6aaf10c2f67c75349309f3adcf1
Heba <hebamagde@gmail.com>
2024-08-15 03:11:59 +0300

    Task 2: Incrementing valuee

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,19 @@
 import redis
 from typing import Any, Callable, Optional, Union
 from uuid import uuid4
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """ Decorator to count how many times methods of the Cache
+    class are called """
+    @wraps(method)
+    def wrapper(self: Any, *args, **kwargs) -> str:
+        """ Wraps called method and adds its call count redis
+        before execution """
+        self._redis.hincrby(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     """ Caching class """
@@ -12,6 +24,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes,  int,  float]) -> str:
         """ Stores data in redis with randomly generated key """
         key = str(uuid4())
